26ece5405a39778a25b4869a18d36db892299688
Matt Gallant <matt@mgallant.com>
2021-02-13 20:14:58 +0000

    Installation of Kubernetes dashboard

--- kubernetes/cluster/init_kube.sh
+++ kubernetes/cluster/init_kube.sh
@@ -6,5 +6,15 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+# Install Calico data store.
+# curl https://docs.projectcalico.org/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
+
+mkdir -p $HOME/bin
+cd $HOME/bin
+curl -O -L https://github.com/projectcalico/calicoctl/releases/download/v3.17.2/calicoctl
+curl -o kubectl-calico -L https://github.com/projectcalico/calicoctl/releases/download/v3.17.2/calicoctl
+chmod +x calicoctl
+chmod +x kubectl-calico
+cd -
 
---
+++ kubernetes/dashboard/install_dashboard.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
+

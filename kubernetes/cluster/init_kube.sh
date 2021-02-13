7663f04df3e07476f3a162c05604b481851298b4
Matt Gallant <matt@mgallant.com>
2021-02-13 19:40:57 +0000

    Add Calico add on as pod network.

--- kubernetes/cluster/init_kube.sh
+++ kubernetes/cluster/init_kube.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-kubeadm init --pod-network-cidr=192.168.0.0/16 --control-plane-endpoint=cluster-endpoint
+kubeadm init --pod-network-cidr=192.168.0.0/16 --control-plane-endpoint=cluster-endpoint | tee -a > cluster_init.log
+
+mv -f $HOME/.kube $HOME/.kube.bak
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f calico.yaml
+

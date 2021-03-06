c4feab735a155009d2ef70755ce853e07435274c
Matt Gallant <matt@mgallant.com>
2021-03-06 16:27:10 -0500

    Update script.

--- MicrosoftOM/ChangeMgmtAgentConfig.ps1
+++ MicrosoftOM/ChangeMgmtAgentConfig.ps1
@@ -1,3 +1,8 @@
+$oldworkspaceId = "335b71f7-88c0-4e05-af6b-b61f7e6ba8da"
+$mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
+$mma.RemoveCloudWorkspace($oldworkspaceId)
+$mma.ReloadConfiguration()
+
 $workspaceId = "b1cde6f6-6bfe-488b-a974-ff44169fa69d"
 $workspaceKey = "OCvxOxA5Frv05d72mUhsDmtwz0LOafo7p2Aj8aeBcKctInly/n3PxcnuP+EmTMsaP9Q1KZ7upyXDHHaWIWiVuw=="
 $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'

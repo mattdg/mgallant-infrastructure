59d49f347d34cd07f604afd7a7d175ea35a91b1f
Matt Gallant <matt@mgallant.com>
2021-03-06 16:30:45 -0500

    Add similar script to reconfigure the Microsoft agent on Linux

---
+++ MicrosoftOM/ChangeMgmtAgentConfig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+/opt/microsoft/omsagent/bin/omsadmin.sh -X
+/opt/microsoft/omsagent/bin/omsadmin.sh -w b1cde6f6-6bfe-488b-a974-ff44169fa69d -s OCvxOxA5Frv05d72mUhsDmtwz0LOafo7p2Aj8aeBcKctInly/n3PxcnuP+EmTMsaP9Q1KZ7upyXDHHaWIWiVuw==
+/opt/microsoft/omsagent/bin/omsadmin.sh -l
+ 

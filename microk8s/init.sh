6218884e97aac29b6458d999d13e060e5d42cd5a
Matt Gallant <matt@mgallant.com>
2021-03-09 00:19:03 -0500

    Add additional components

--- microk8s/init.sh
+++ microk8s/init.sh
@@ -4,4 +4,8 @@
 # First script to run after installing the microk8s cluster and joining the initial nodes.
 #
 
-microk8s enable dns dashboard rbac
+microk8s enable dns ingress rbac storage 
+microk8s enable portainer
+
+echo Enabling MetalLB. Specify range as 192.168.1.240-192.168.1.249
+microk8s enable metallb
